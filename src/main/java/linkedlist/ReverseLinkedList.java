909bbdf9b2b85db177c034c98bb4e324a659cf4a
chariotsx <digital.osx@gmail.com>
2022-04-10 16:55:43 -0500

    Solving Reverse Linked List

---
+++ src/main/java/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,44 @@
+package linkedlist;
+
+import common.Solution;
+import common.datastructures.linkedlist.LinkedList;
+import common.datastructures.linkedlist.LinkedListNode;
+import lombok.Getter;
+
+@Getter
+public class ReverseLinkedList extends Solution<LinkedListNode> {
+
+  private final LinkedList<Integer> list;
+
+  public ReverseLinkedList(LinkedList<Integer> list) {
+    this.list = list;
+  }
+
+  @Override
+  protected void initializeSolution() {
+    this.title = "Reverse Linked List";
+    this.url = "https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/";
+    this.notes = "Iterative solution";
+  }
+
+  @Override
+  public LinkedListNode solve() {
+    LinkedListNode<Integer> currentNode = this.list.getHeadNode();
+    LinkedListNode<Integer> previousNode = null;
+
+    // For LinkedList consistency purposes
+    this.list.setTail(this.list.getHeadNode());
+
+    while (currentNode != null) {
+      LinkedListNode<Integer> nextNode = currentNode.getNextNode();
+      currentNode.setNextNode(previousNode);
+      previousNode = currentNode;
+      currentNode = nextNode;
+    }
+
+    // For LinkedList consistency purposes
+    this.list.setHead(previousNode);
+
+    return previousNode;
+  }
+}
---
+++ src/test/java/linkedlist/ReverseLinkedListTest.java
@@ -0,0 +1,57 @@
+package linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import common.datastructures.linkedlist.LinkedList;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReverseLinkedListTest {
+
+  private ArrayList<ReverseLinkedList> solutions;
+  private ArrayList<LinkedList<Integer>> expectedResults;
+
+  @BeforeEach
+  void setUp() {
+    solutions = new ArrayList<>();
+    expectedResults = new ArrayList<>();
+
+    // Test 1
+    LinkedList<Integer> linkedListTest1 = new LinkedList<>(new Integer[]{1, 2, 3, 4, 5});
+    LinkedList<Integer> expectedLinkedListTest1 = new LinkedList<>(new Integer[]{5, 4, 3, 2, 1});
+
+    solutions.add(
+        new ReverseLinkedList(linkedListTest1));
+    expectedResults.add(expectedLinkedListTest1);
+
+    // Test 2
+    LinkedList<Integer> linkedListTest2 = new LinkedList<>();
+    LinkedList<Integer> expectedLinkedListTest2 = new LinkedList<>();
+
+    solutions.add(
+        new ReverseLinkedList(linkedListTest2));
+    expectedResults.add(expectedLinkedListTest2);
+
+    // Test 3
+    LinkedList<Integer> linkedListTest3 = new LinkedList<>(new Integer[]{1, 2});
+    LinkedList<Integer> expectedLinkedListTest3 = new LinkedList<>(new Integer[]{2, 1});
+
+    solutions.add(
+        new ReverseLinkedList(linkedListTest3));
+    expectedResults.add(expectedLinkedListTest3);
+  }
+
+  @Test
+  void solveTest() {
+    int index = 0;
+    for (ReverseLinkedList solution : this.solutions) {
+      solution.solve();
+      assert solution.getList().equals(this.expectedResults.get(index)) :
+          "Test[" + (index + 1) + "] " +
+              "Result: " + solution.getList().toString() + " Expected: " +
+              this.expectedResults.get(index).toString();
+      index++;
+    }
+  }
+}
